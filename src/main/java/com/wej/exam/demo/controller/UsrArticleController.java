8148f3e02705f3ac9be423a275d458fc70841439
Eunju0114 <wangeunju0114@gmail.com>
2022-06-28 20:01:00 +0900

    테스트용 게시물 10개 자동생성

--- src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
@@ -17,9 +17,26 @@ public class UsrArticleController {
 	
 	public UsrArticleController () {
 		articles = new ArrayList<>();
+		makeTestData();
 	}
 	
 	
+	private void makeTestData() {
+
+		for(int i = 1; i <=10; i++){
+			id ++;
+			String title ="제목" + i;
+			String body ="내용" + i;
+			
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			
+		}
+		
+	}
+
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -32,10 +49,17 @@ public class UsrArticleController {
 	}
 	
 
-	@RequestMapping("/usr/article/getArticle")
+	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticle() {
+	public List<Article> getArticles() {
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/deleteArticle")
+	@ResponseBody
+	public String deleteArticle(int id) {
+
+		return id + "번 게시물을 삭제하였습니다.";
 	}
 }
 

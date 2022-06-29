9e0854dd20aa7c758354b9f915d09aad881da3fd
Eunju0114 <wangeunju0114@gmail.com>
2022-06-29 20:33:14 +0900

    ArticleService 생성후 서비스로직옮김

--- src/main/java/com/wej/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrHomeController.java
@@ -7,23 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UsrHomeController {
 
-	private int count;
-	
-	public UsrHomeController() {
-		count = -1;
-	}
-	
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public int getCount() {
-		count ++;
-		return count;
-	}
-	
-	@RequestMapping("/usr/home/setCount")
-	@ResponseBody
-	public String setCount(int count) {
-		this.count = count;
-		return "count 값이 " + this.count + "으로 초기화 되었습니다.";
+	public String getString() {
+		return "안녕하세요";
 	}
 }

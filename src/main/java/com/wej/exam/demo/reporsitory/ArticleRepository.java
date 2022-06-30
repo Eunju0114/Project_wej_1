5ab6852b48c4d0cd0683403a058c560b57ff2125
Eunju0114 <wangeunju0114@gmail.com>
2022-06-30 16:05:49 +0900

    delete update select 관련 쿼리를 메서드와 연결

--- src/main/java/com/wej/exam/demo/reporsitory/ArticleRepository.java
+++ src/main/java/com/wej/exam/demo/reporsitory/ArticleRepository.java
@@ -2,24 +2,31 @@ package com.wej.exam.demo.reporsitory;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.wej.exam.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
 
-	public Article writeArticle(String title, String body);
+	// @Insert("insert into article set title = #{title}, `body`= #{body}, regDate = now(), updateDate = now()")
+	public Article writeArticle(@Param("title") String title,@Param("body") String body);
 
 	@Select("select * from article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	public void deleteArticle(int id);
+	@Delete("delete from article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 
-	public void modifyArticle(int id, String title, String body);
+	@Update("update article set title = #{title}, `body` = #{body}, updateDate = now() WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id,@Param("title") String title,@Param("body") String body);
 
+	@Select("select * from article order by id desc")
 	public List<Article> getArticles();
 
 }

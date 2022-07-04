890c15d82b7837cb0bd5ea2991f110c014581930
Eunju0114 <wangeunju0114@gmail.com>
2022-07-04 18:18:03 +0900

    ,회원가입 처리를 위해 UsrMemberController, MemberService, MemberRepository 추가, 테이블 추가, 회원가입 처리'

---
+++ src/main/java/com/wej/exam/demo/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.wej.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.wej.exam.demo.repository.MemberRepository;
+
+
+@Service
+public class MemberService {
+
+	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+	}
+
+}

611b50d119bc7ade655b19cadf5c2acbea82d836
Eunju0114 <wangeunju0114@gmail.com>
2022-07-05 21:47:06 +0900

    C:/Program Files/Git/usr/member/doJoin 에 ResultData 적용

--- src/main/java/com/wej/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.wej.exam.demo.service.MemberService;
 import com.wej.exam.demo.utill.Ut;
 import com.wej.exam.demo.vo.Member;
+import com.wej.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -24,41 +25,37 @@ public class UsrMemberController {
 			String email) {
 
 		if (Ut.empty(loginId)) {
-			return "loginId(을)를 입력해주세요.";
+			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
 		}
 
 		if (Ut.empty(loginPw)) {
-			return "loginPw(을)를 입력해주세요.";
+			return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
 		}
 
 		if (Ut.empty(name)) {
-			return "name(을)를 입력해주세요.";
+			return ResultData.from("F-3", "name(을)를 입력해주세요.");
 		}
 
 		if (Ut.empty(nickname)) {
-			return "nickname(을)를 입력해주세요.";
+			return ResultData.from("F-4", "nickname(을)를 입력해주세요.");
 		}
 
 		if (Ut.empty(cellphoneNo)) {
-			return "cellphoneNo(을)를 입력해주세요.";
+			return ResultData.from("F-5", "cellphoneNo(을)를 입력해주세요.");
 		}
 
 		if (Ut.empty(email)) {
-			return "email(을)를 입력해주세요.";
+			return ResultData.from("F-6", "email(을)를 입력해주세요.");
 		}
 
-		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		if (id == -1) {
-			return Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId);
-		}
-		
-		if (id == -2) {
-			return Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email);
+		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		if (joinRd.isFail()) {
+			return joinRd;
 		}
 
-		Member member = memberService.getMemberById(id);
+		Member member = memberService.getMemberById((int)joinRd.getData1());
 
-		return member;
+		return ResultData.newData(joinRd, member);
 	}
 
 }

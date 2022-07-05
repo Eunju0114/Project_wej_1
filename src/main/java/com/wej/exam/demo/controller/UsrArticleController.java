36eb9ce01d560ab94cb9ac5b854218a51f342ba7
Eunju0114 <wangeunju0114@gmail.com>
2022-07-05 22:48:23 +0900

    Article 클래스에 regDate, updateDate 필드 추가

--- src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
@@ -34,7 +34,7 @@ public class UsrArticleController {
 		int id = (int) writeArticleRd.getData1();
 
 		Article article = articleService.getArticle(id);
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+		return ResultData.newData(writeArticleRd, article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")

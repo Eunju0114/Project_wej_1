90ce6d16d6abb3efef72e0b67eda3b7816f7c556
Eunju0114 <wangeunju0114@gmail.com>
2022-07-05 17:22:34 +0900

    회원가입 시 이름+이메일 중복 체크

--- src/main/java/com/wej/exam/demo/service/MemberService.java
+++ src/main/java/com/wej/exam/demo/service/MemberService.java
@@ -5,26 +5,37 @@ import org.springframework.stereotype.Service;
 import com.wej.exam.demo.repository.MemberRepository;
 import com.wej.exam.demo.vo.Member;
 
-
 @Service
 public class MemberService {
 
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		// 로그인 아이디 중복 체크
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if( oldMember != null ) {
+
+		if (oldMember != null) {
 			return -1;
 		}
-		
+
+		// 이름 + 이메일 중복 체크
+		oldMember = getMemberByNameEmail(name, email);
+
+		if (oldMember != null) {
+			return -2;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLastInsertId();
-		
+
+	}
+
+	private Member getMemberByNameEmail(String name, String email) {
+		return memberRepository.getMemberByNameAndEmail(name, email);
 	}
 
 	private Member getMemberByLoginId(String loginId) {

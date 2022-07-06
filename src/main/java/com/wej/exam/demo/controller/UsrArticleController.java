fee4b2f78eccbe115d0b902aefb60c602cb4fa03
Eunju0114 <wangeunju0114@gmail.com>
2022-07-06 17:57:44 +0900

    게시물 수정시 권한체크(오류)

--- src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
@@ -107,17 +107,36 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData doModify(int id, String title, String body) {
+	public ResultData<Article> doModify(HttpSession httpsession, int id, String title, String body) {
+		
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+
+		if (httpsession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpsession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+		
 
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
+		
+		ResultData actorCanModifyRd = articleService.actorCanModify(loginedMemberId, article);
+	
+		if(actorCanModifyRd.isFail()) {
+			return actorCanModifyRd;
+		}
+		
+		return articleService.modifyArticle(id, title, body);
 
-		articleService.modifyArticle(id, title, body);
-
-		return ResultData.from("S-1", Ut.f("%d번 번 게시물을 수정하였습니다.", id));
+	
 
 	}
 	// 액션 메서드 끝

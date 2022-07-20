3bc108fa47924eb4c613e569f1f23f73c4b49028
Eunju0114 <wangeunju0114@gmail.com>
2022-07-20 21:23:35 +0900

    NeedLogin입터셉터가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/wej/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/wej/exam/demo/config/MyWebMvcConfigurer.java
@@ -9,30 +9,23 @@ import com.wej.exam.demo.interceptor.BeforeActionInterceptor;
 import com.wej.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
-public class MyWebMvcConfigurer implements WebMvcConfigurer{
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	// beforeActionInterceptor 인터셉터 불러오기
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
-	
+
 	@Autowired
 	NeedLoginInterceptor needLoginInterceptor;
-
 
 	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
 
-		
-		registry.addInterceptor(needLoginInterceptor)
-		.addPathPatterns("/usr/article/write")
-		.addPathPatterns("/usr/article/dowrite")
-		.addPathPatterns("/usr/article/modify")
-		.addPathPatterns("/usr/article/domodify")
-		.addPathPatterns("/usr/article/doDelete");
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
 	}
-	
-	
-	
-	
+
 }

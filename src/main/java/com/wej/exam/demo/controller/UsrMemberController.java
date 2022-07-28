281c51ca6dbcf84997e78de49e40f4307e0f1bae
Eunju0114 <wangeunju0114@gmail.com>
2022-07-28 18:03:07 +0900

    로그인 로그아웃 처리에 Rq 적용

--- src/main/java/com/wej/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrMemberController.java
@@ -1,5 +1,6 @@
 package com.wej.exam.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import com.wej.exam.demo.service.MemberService;
 import com.wej.exam.demo.utill.Ut;
 import com.wej.exam.demo.vo.Member;
 import com.wej.exam.demo.vo.ResultData;
+import com.wej.exam.demo.vo.Rq;
 
 @Controller
 public class UsrMemberController {
@@ -61,21 +63,18 @@ public class UsrMemberController {
 	}
 
 	@RequestMapping("/usr/member/login")
-	public String showLogin(HttpSession httpSession) {
+	public String showLogin() {
 		return "/usr/member/login";
 	}
 
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(HttpSession httpSession, String loginId, String loginPw) {
+	public String doLogin(HttpServletRequest req, String loginId, String loginPw) {
 
-		boolean isLogined = false;
-
-		if (httpSession.getAttribute("loginedMemberId") != null) {
-			isLogined = true;
-		}
-
-		if (isLogined) {
+		Rq rq = (Rq) req.getAttribute("rq");
+		
+		
+		if (rq.isLogined()) {
 			return Ut.jsHistoryBack("이미 로그인 되었습니다.");
 
 		}
@@ -99,23 +98,24 @@ public class UsrMemberController {
 			return Ut.jsHistoryBack("비밀번호 오류.");
 		}
 
-		httpSession.setAttribute("loginedMemberId", member.getId());
+		rq.login(member);
+		
 		return Ut.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), "/");
 	}
 
 	@RequestMapping("/usr/member/doLogout")
 	@ResponseBody
-	public Object doLogout(HttpSession httpSession) {
-		boolean isLogined = false;
-
-		if (httpSession.getAttribute("loginedMemberId") == null) {
-			isLogined = true;
-		}
-		if (isLogined) {
+	public Object doLogout(HttpServletRequest req) {
+		
+		Rq rq = (Rq) req.getAttribute("rq");
+		
+		
+		if (rq.isLogined()) {
 			return ResultData.from("S-1", "이미 로그아웃 상태입니다.");
 		}
 
-		httpSession.removeAttribute("loginedMemberId");
+		rq.logout();
+		
 
 		return ResultData.from("S-2", "로그아웃 되었습니다.");
 
